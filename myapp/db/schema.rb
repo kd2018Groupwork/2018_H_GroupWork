562d97572acc6d74bb9d03bbfb8dbb7580734a74
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-12-06 16:52:23 +0900

    [change] 不要なデータベースを削除

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181130053017) do
-
-  create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20181206074417) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
