018f080d5d973e0e60e3fe74374ff03a5bf88a5d
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-12-07 13:31:37 +0900

    [change] Reset Database

--- myapp/db/migrate/20181130042538_create_users.rb
+++ myapp/db/migrate/20181130042538_create_users.rb
@@ -1,10 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.integer :user_id
+      t.string :user_name
+      t.string :password
       t.string :email
+      t.integer :generation
 
-      t.timestamps
     end
   end
 end
