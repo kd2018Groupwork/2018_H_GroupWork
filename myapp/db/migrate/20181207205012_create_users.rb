56de63d481add4342e0b2acf1844933ea3d1b345
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-12-08 06:50:10 +0900

    [Add] User Profile

--- myapp/db/migrate/20181207205012_create_users.rb
+++ myapp/db/migrate/20181207205012_create_users.rb
@@ -2,9 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :user_name
-      t.string :password
       t.string :email
-      t.string :generation
+      t.integer :generation
 
       t.timestamps
     end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181207081421) do
+ActiveRecord::Schema.define(version: 20181207211426) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "area_name"
@@ -36,11 +36,11 @@ ActiveRecord::Schema.define(version: 20181207081421) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "user_name"
-    t.string "password"
     t.string "email"
-    t.string "generation"
+    t.integer "generation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
