3956f1d45f610b211db221eae7fd1796ac911ccc
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-12-08 18:00:01 +0900

    [Add] Signup form

--- myapp/app/controllers/users_controller.rb
+++ myapp/app/controllers/users_controller.rb
@@ -3,7 +3,29 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     debugger
   end
-  def new
 
+  def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private                                                                       
+
+    def user_params
+      params.require(:user).permit(
+        :user_name, 
+        :email,
+        :generation,
+        :password, 
+        :password_confirmation)
+    end
 end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20181207211426) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "user_name"
     t.string "email"
-    t.integer "generation"
+    t.string "generation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
