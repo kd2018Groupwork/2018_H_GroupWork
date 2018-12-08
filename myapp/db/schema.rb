6ff455055f9cfd3e474b3ed69d86955511faf1cb
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-12-08 20:14:37 +0900

    [Add] User Login

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181207211426) do
+ActiveRecord::Schema.define(version: 20181208105527) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "area_name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20181207211426) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
