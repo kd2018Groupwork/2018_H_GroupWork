76554d0f5a37b8e14c67ce2d9b25e0f4e1091099
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-12-12 12:10:20 +0900

    [Add] 新規登録　k完成

--- myapp/app/controllers/users_controller.rb
+++ myapp/app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(
         :user_name, 
         :email,
-        :generation,
         :password, 
         :password_confirmation)
     end
