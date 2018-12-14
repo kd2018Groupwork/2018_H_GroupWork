e29fdb9891e167c9ef3e6dab274c6ac5d0e8db15
masashi nishida <kd1229417@st.koedenshi.ac.jp>
2018-12-14 13:12:13 +0900

    fixed a routes.rb

--- web_rails/myapp/config/routes.rb
+++ web_rails/myapp/config/routes.rb
@@ -1,26 +1,24 @@
 Rails.application.routes.draw do
+  root                        'static_pages#home'
+  get     'help'           => 'static_pages#help'
+  get     'about'          => 'static_pages#about'
 
-  get 'planning/new'
+  get     'planning/new'
+  get     'planning/reference'
 
-  get 'planning/reference'
-
-  get     'search'    => 'search#search'
-
+  get     'search'         => 'search#search'
   get     'search_result'  => 'search#search_result'
-
   get     'search_detail'  => 'search#search_detail'
 
-  root                    'static_pages#home'
-  get     'help'      =>  'static_pages#help'
-  get     'about'     =>  'static_pages#about'
-  get     'signup'    =>  'users#new'
-  get     'login'     =>  'sessions#new'
-  post    'login'     =>  'sessions#create'
-  delete  'logout'    =>  'sessions#destroy'
-  get                     'sessions/new'
-  get     'add_spot'  =>  'tourist_spot#new'
-  post    'spots'     =>  'tourist_spot#create'
-  get     'complete_spot' =>  'tourist_spot#complete'
+  get     'signup'         => 'users#new'
+  get     'login'          => 'sessions#new'
+  get                         'sessions/new'
+  post    'login'          => 'sessions#create'
+  delete  'logout'         => 'sessions#destroy'
+
+  get     'add_spot'       => 'tourist_spot#new'
+  post    'spots'          => 'tourist_spot#create'
+  get     'complete_spot'  => 'tourist_spot#complete'
 
   resources :users
 
