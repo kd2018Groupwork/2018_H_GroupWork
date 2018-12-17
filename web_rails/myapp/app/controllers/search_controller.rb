8d9107c7485077a3f788347f1a33f3951757eed3
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2018-12-17 09:46:53 +0900

    [add] favorite

--- web_rails/myapp/app/controllers/search_controller.rb
+++ web_rails/myapp/app/controllers/search_controller.rb
@@ -6,5 +6,13 @@ class SearchController < ApplicationController
   end
 
   def search_detail
+    @a = 1
+    @detail = Spot.find_by_sql(['
+      select spot_name , prefs.name as pref_name , cities.name as city_name , productes.product_name , spot_address , evalution , details , image_path 
+      from spots,prefs,cities,productes 
+      where spots.id = :id
+      and spots.prefs_id = prefs.id 
+      and spots.cities_id = cities.id 
+      and spots.product_id = productes.id',{id: @a}])
   end
 end
