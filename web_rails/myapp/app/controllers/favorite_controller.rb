7e6c64636cbc55a18f241fe9c73f28a2f803d3e6
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2018-12-19 11:00:32 +0900

    [change] ヘッダー  お気に入りページ遷移追加

--- web_rails/myapp/app/controllers/favorite_controller.rb
+++ web_rails/myapp/app/controllers/favorite_controller.rb
@@ -1,16 +1,26 @@
 class FavoriteController < ApplicationController
+  def show
+    #params[]はString型
+    if current_user.id === params[:user_id].to_i
+      
+    else
+      redirect_to root_path
+    end
+  end
+
   def create
-    if Favorite.create(user_id: current_user.id , spot_id: params[:'spot_id'])
-      redirect_to search_detail_path(spot_id: params[:spot_id])
+    @spot = params[:spot_id]
+    if Favorite.create(user_id: current_user.id , spot_id: @spot)
+      redirect_to search_detail_path(spot_id: @spot)
     else  
       redirect_to root_path
     end
   end
 
   def destroy
-    if favorite = Favorite.find_by(user_id: current_user.id , spot_id: params[:'spot_id'])
+    if favorite = Favorite.find_by(user_id: current_user.id , spot_id: @spot)
       favorite.delete
-      redirect_to search_detail_path(spot_id: params[:spot_id])
+      redirect_to search_detail_path(spot_id: @spot)
     else
       redirect_to root_path
     end
