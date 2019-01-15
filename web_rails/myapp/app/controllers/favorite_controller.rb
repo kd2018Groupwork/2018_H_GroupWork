070c9853e189cda57154b7d65d3c649887321328
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-01-15 14:48:05 +0900

    [add]ピックアップ参照ページのデザイン修正

--- web_rails/myapp/app/controllers/favorite_controller.rb
+++ web_rails/myapp/app/controllers/favorite_controller.rb
@@ -17,7 +17,9 @@ class FavoriteController < ApplicationController
     if favorite = Favorite.find_by(user_id: current_user.id , spot_id: params[:spot_id])
       favorite.delete
       User.where('id = ?', @user_spot.user_id).update_all("rating = rating - 5")
-      #redirect_to search_detail_path(spot_id: @spot)
+      if URI(request.referer).path == fav_show_path(user_id: current_user.id)
+        redirect_to fav_show_path(user_id: current_user.id)
+      end
     else
       redirect_to root_path
     end
@@ -27,7 +29,6 @@ class FavoriteController < ApplicationController
 
   def set_variables
     if params[:spot_id]
-      puts params[:spot_id]
       @spt = Spot.find(params[:spot_id])
       @id_name = "#fav-link-#{@spt.id}"
     end
