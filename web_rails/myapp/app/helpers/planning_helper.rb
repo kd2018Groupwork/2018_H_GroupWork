b5f22c7dca9e21a5c12a927966291f4558ee63ee
astro1010 <kd1229417@st.koedenshi.ac.jp>
2019-01-22 10:03:44 +0900

    [Change] 計画表の聖地リストにその他追加

--- web_rails/myapp/app/helpers/planning_helper.rb
+++ web_rails/myapp/app/helpers/planning_helper.rb
@@ -1,2 +1,16 @@
 module PlanningHelper
+
+  def get_favorite_spots()
+    hash =  {}
+    (Favorite.where(user_id: session[:user_id])).each do |data|
+      hash.merge!(data.spot_id => (Spot.find_by(id: data.spot_id)).spot_name)
+    end
+    hash
+  end
+  
+  def add_other_value_in_hash(hash)
+    hash.store(:other_spots,'その他')
+    hash
+  end
+
 end
