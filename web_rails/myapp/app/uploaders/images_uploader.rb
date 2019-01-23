8734215ac76a674d09e3b5de17e28c81401934a4
astro1010 <kd1229417@st.koedenshi.ac.jp>
2019-01-23 14:24:32 +0900

    [Change] プロフィール画像バグ修正

--- web_rails/myapp/app/uploaders/images_uploader.rb
+++ web_rails/myapp/app/uploaders/images_uploader.rb
@@ -1,4 +1,6 @@
 class ImagesUploader < CarrierWave::Uploader::Base
+  #require 'digest/md5'
+  
   def default_url(*args)
     "/images/" + [version_name, "default.png"].compact.join('_')
   end
@@ -11,12 +13,13 @@ class ImagesUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::RMagick
 
-  process :resize_to_limit => [700, 700]
+  #process :resize_to_limit => [700, 700]
+  process :resize_to_limit => [100, 100]
   process :convert => 'jpg'
   
-  version :thumb do
-    process :resize_to_limit => [100, 100]
-  end
+  #version :thumb do
+  #  process :resize_to_limit => [100, 100]
+  #end
   
   def extension_white_list
     %w(jpg jpeg gif png)
@@ -24,14 +27,12 @@ class ImagesUploader < CarrierWave::Uploader::Base
   
   def filename
     super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  end
-  
-  def filename
     time = Time.now
     name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
     name.downcase
+    #var = Digest::MD5.new.update().to_s
     var = "#{SecureRandom.hex(10)}"
-    @name ||= "#{var}-" + "#{name}" + ".jpg" if original_filename.present?
+    @name ||= "#{var}-" + "#{name}" if original_filename.present?
   end
   
 end
