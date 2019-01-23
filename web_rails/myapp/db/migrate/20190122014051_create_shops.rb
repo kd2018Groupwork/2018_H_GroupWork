0253e34b5d4b5d256b18c47acdb1abcdf078aa80
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-01-23 13:23:03 +0900

    [change]カラム変更

--- web_rails/myapp/db/migrate/20190122014051_create_shops.rb
+++ web_rails/myapp/db/migrate/20190122014051_create_shops.rb
@@ -2,6 +2,8 @@ class CreateShops < ActiveRecord::Migration[5.1]
   def change
     create_table :shops do |t|
       t.integer :spot_id
+      t.string  :postcode1
+      t.string  :postcode2
       t.text  :address
       t.string :shop_name
       t.text :detail
