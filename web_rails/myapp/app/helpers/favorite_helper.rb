4d7e387e5937cc25e439465cb1c0c326d77e6d75
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-01-25 14:12:52 +0900

    [Edit] query

--- web_rails/myapp/app/helpers/favorite_helper.rb
+++ web_rails/myapp/app/helpers/favorite_helper.rb
@@ -1,2 +1,10 @@
 module FavoriteHelper
+  def favotite_all
+    @favorite ||= Favorite.all
+  end
+
+  def is_favorite_exist?(user_id, spot_id)
+    favorits = favotite_all
+    !favorits.exists?(user_id: user_id, spot_id: spot_id)
+  end
 end
